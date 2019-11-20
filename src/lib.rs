d73cad228d5c198872c3a448148a81c22f301f2e
axler8tor <axl.mattheus@gmail.com>
2019-11-20 18:13:42 +1300

    Added library.

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+pub enum Colors {
+    None,
+    Black,
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Cyan,
+    Voilet,
+    Magenta,
+    White,
+}
+
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Decoration {
+
+}
+
+impl Decoration {
+
+}
+
+pub fn run() {
+
+}

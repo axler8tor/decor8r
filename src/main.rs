2cfea30273f7db82717cab9bc96c9edcb5efe337
axler8tor <axl.mattheus@gmail.com>
2019-11-22 13:52:17 +1300

    Updated main file.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
+#[macro_use]
 extern crate clap;
+
 
 fn main() {
     let clargs = clap::App::new("decor8r")
-        .author("Axl Mattheus, decor8r@axl.tech")
-        .version("0.1.0")
-        .about("Decorate terminal-based status lines or prompts.")
-        .long_about(
-            "Produce decorated zsh, tmux and neovim status lines or command prompts. Inspried by powerline."
-        )
-        .arg(
-            clap::Arg::with_name("color")
-                .help("Color display mode.")
-                .long("color")
-                .short("c")
-                .alias("colour")
-                .value_name("MODE")
-                .possible_values(&["auto", "8", "16", "256"])
-                .global(true)
-        )
-        .subcommand(
-            clap::SubCommand::with_name("zsh")
-                .about("Decorate zsh command lines.")
-        )
-        .subcommand(
-            clap::SubCommand::with_name("tmux")
-                .about("Decorate tmux sesions.")
-        )
-        .subcommand(
-            clap::SubCommand::with_name("nvim")
-                .about("Decorate neovim status lines.")
-        )
-        .get_matches();
+            .author("Axl Mattheus, decor8r@axl.tech")
+            .version(crate_version!())
+            .about("Decorate terminal-based status lines or prompts.")
+            .long_about(
+                "Produce decorated zsh, tmux and neovim status lines or command prompts. Inspried by powerline."
+            )
+            .arg(
+                clap::Arg::with_name("color")
+                    .help("Color display mode.")
+                    .takes_value(true)
+                    .long("color")
+                    .short("c")
+                    .alias("colour")
+                    .value_name("MODE")
+                    .possible_values(&["auto", "8", "16", "256"])
+                    .default_value("16")
+                    .global(true)
+            )
+            .subcommand(
+                clap::SubCommand::with_name("zsh")
+                    .about("Decorate zsh command lines.")
+            )
+            .subcommand(
+                clap::SubCommand::with_name("tmux")
+                    .about("Decorate tmux sesions.")
+            )
+            .subcommand(
+                clap::SubCommand::with_name("nvim")
+                    .about("Decorate neovim status lines.")
+            ).get_matches();
 
     match clargs.subcommand_name() {
         Some("zsh") => println!("zsh"),

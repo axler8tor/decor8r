5fd53184cc8f67691ed367510f2c4b6c2189d848
axler8tor <axl.mattheus@gmail.com>
2019-12-22 21:02:41 +1300

    [wip] Get to know ANSI codes and Elixir.

--- lib/decorator.exs
+++ lib/decorator.exs
@@ -1,5 +1,22 @@
 defmodule Decorator do
-
   def get_path do
+    cwd = File.cwd()
+    home = System.get_env("HOME")
+    %{cwd: cwd, home: home}
+  end
+
+  def compress_path(cwd, home) do
+    String.replace(cwd, home, "~")
+  end
+
+  def decorate(path) do
+    bg_green = "\\033[32m"
+    bg_normal = "\\033[0m"
+    bg_green <> path <> bg_normal
   end
 end
+
+%{cwd: {:ok, cwd}, home: home} = Decorator.get_path
+path = Decorator.compress_path(cwd, home)
+decorated_path = Decorator.decorate(path)
+IO.puts decorated_path

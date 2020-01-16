7aebfc20c113c2d76971041ece1bf93f84c81e30
axler8tor <axl.mattheus@gmail.com>
2020-01-16 15:11:09 +1300

    Tune formatting rules

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Decorator.MixProject do
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
       {:toml, "~> 0.6.1"},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 

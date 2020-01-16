7f742ee8987b1bc96e551af421b8fc2978558651
axler8tor <axl.mattheus@gmail.com>
2020-01-16 14:20:48 +1300

    Add documentation functionality

--- lib/decorator/config/key.ex
+++ lib/decorator/config/key.ex
@@ -1,10 +1,10 @@
 defmodule Decorator.Config.Key do
   @moduledoc """
-  Key provides a convinience sigil to convert paths, as used in key maps into a
-  list of atoms to pass ot Kernet.get_in/2.
+  #{__MODULE__} provides a convinience sigil – `sigil_k/2` – to convert paths,
+  as used in keyword maps into a list of atoms to pass ot `Kernet.get_in/2`.
 
   ## Examples
-
+    ``` Elixir
     iex> map = %{a: %{deeply: %{nested: %{value: true}}}}
     %{a: %{deeply: %{nested: %{value: true}}}}
 
@@ -14,20 +14,35 @@ defmodule Decorator.Config.Key do
     iex> get_in(map, [:a, :deeply, :nested, :value])
     true
 
+    iex> import #{__MODULE__}
     iex> get_in(map, ~k[a.deeply.nested.value])
     true
+    ```
   """
+
+  @typedoc """
+  A fully qualified path of terms, separated by dots.
+
+  ## Examples
+    ` part.another_part.last_part`
+  """
+  @type key :: String.t()
+
+  @typedoc """
+  A list of options. Currently ignored and aliased to `any`.
+  """
+  @type opts :: any
 
   @doc """
   Convers a dot separated path of terms into a list of atoms.
 
   ## Examples
-
+    ``` Elixir
     iex> ~k[path.to.value]
     [:path, :to, :value]
-
+    ```
   """
-  @spec sigil_k(String.t(), any) :: [atom]
+  @spec sigil_k(key, opts) :: list(atom)
   def sigil_k(key, _opts) when is_binary(key) do
     key |> String.split(".") |> Enum.map(&String.to_atom(&1))
   end
--- mix.exs
+++ mix.exs
@@ -8,11 +8,21 @@ defmodule Decorator.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+
+      # QC
       aliases: aliases(),
       dialyzer: [
         plt_file: {:no_warn, "_dialyzer/plts/dialyzer.plt"},
         ignore_warnings: ".dialyzer_ignore.exs"
-      ]
+      ],
+
+      # Documentation
+      source_url: "https://github.com/axler8tor/decor8r",
+      docs: [
+        extras: [
+          "README.md"
+        ]
+      ],
     ]
   end
 
@@ -27,7 +37,8 @@ defmodule Decorator.MixProject do
     [
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
-      {:toml, "~> 0.6.1"}
+      {:toml, "~> 0.6.1"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     ]
   end
 

7a2e93551d98474ffc0f34474073bb565d9a8cb4
axler8tor <axl.mattheus@gmail.com>
2020-01-18 08:49:55 +1300

    Clean up redundant code

--- lib/decorator/application.ex
+++ lib/decorator/application.ex
@@ -3,15 +3,13 @@ defmodule Decorator.Application do
 
   use Application
 
-  @this __MODULE__
-
   def start(_type, _args) do
     children = [
       Decorator.Config.Supervisor,
       Decorator.Shell.Supervisor
     ]
 
-    opts = [strategy: :one_for_one, name: @this]
+    opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
   end
 end

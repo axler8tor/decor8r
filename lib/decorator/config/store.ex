c587626a57d3386bc0bcff260ec2743ea55e59e8
axler8tor <axl.mattheus@gmail.com>
2020-01-18 08:54:24 +1300

    Design TOML configuration file

--- lib/decorator/config/store.ex
+++ lib/decorator/config/store.ex
@@ -74,44 +74,12 @@ defmodule Decorator.Config.Store do
 
   defp initialise(store) do
     with :ok <- File.mkdir_p(Path.dirname(store)),
-         :ok <- File.write(store, file_content()) do
+         :ok <- File.write(store, config_file()) do
       :ok
     else
       {:error, error} -> {:error, error}
     end
   end
 
-  defp file_content do
-    ~S"""
-    [about]
-    version = "0.1.5"
-    licence = "https://unlicense.org/"
-    documentation = "https://decor8r.readthedocs.io/"
-    homepage = "https://decor8r.axler8r.io/"
-    source = "https://decor8r.axler8r.io/source/"
-    bugs = "decor8r-bugs@axler8r.io"
-
-    [theme]
-    name = "Material Palenight"
-
-    [shell]
-    theme = "Material Pale Night"
-
-    [shell.listener]
-    port = 65521
-    socket = "/tmp/decor8r.sock"
-
-    [shell.decoration]
-    segments = ["vi", "user", "path", "venv", "git", "status"]
-
-    [shell.segment]
-    [shell.segment.vi]
-    [shell.segment.user]
-    [shell.segment.path]
-    [shell.segment.venv]
-    [shell.segment.git]
-    [shell.segment.status]
-
-    """
-  end
+  defp config_file, do: File.read!(Path.join(:code.priv_dir(:decorator), "config.toml"))
 end

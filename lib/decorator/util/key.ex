a4717a38477541bc64dfdaf747e25ab33f4c0119
axler8tor <axl.mattheus@gmail.com>
2020-01-20 10:39:48 +1300

    Update documentation

--- lib/decorator/util/key.ex
+++ lib/decorator/util/key.ex
@@ -3,28 +3,30 @@ defmodule Decorator.Util.Key do
   #{__MODULE__} provides a convinience sigil – `sigil_k/2` – to convert paths,
   as used in keyword maps into a list of atoms to pass ot `Kernet.get_in/2`.
 
+
   ## Examples
-    ``` Elixir
-    iex> map = %{a: %{deeply: %{nested: %{value: true}}}}
-    %{a: %{deeply: %{nested: %{value: true}}}}
+      iex> map = %{a: %{deeply: %{nested: %{value: true}}}}
+      %{a: %{deeply: %{nested: %{value: true}}}}
 
-    iex> map.a.deeply.nested.value
-    true
+      iex> map.a.deeply.nested.value
+      true
 
-    iex> get_in(map, [:a, :deeply, :nested, :value])
-    true
+      iex> get_in(map, [:a, :deeply, :nested, :value])
+      true
 
-    iex> import #{__MODULE__}
-    iex> get_in(map, ~k[a.deeply.nested.value])
-    true
-    ```
+      iex> import #{__MODULE__}
+      #{__MODULE__}
+      iex> get_in(map, ~k[a.deeply.nested.value])
+      true
   """
 
   @typedoc """
   A fully qualified path of terms, separated by dots.
 
+
   ## Examples
-    ` part.another_part.last_part`
+
+      part.another_part.last_part
   """
   @type key :: String.t()
 
@@ -36,13 +38,13 @@ defmodule Decorator.Util.Key do
   @doc """
   Convers a dot separated path of terms into a list of atoms.
 
+
   ## Examples
-    ``` Elixir
-    iex> ~k[path.to.value]
-    [:path, :to, :value]
-    ```
+
+      iex> ~k[path.to.value]
+      [:path, :to, :value]
   """
-  @spec sigil_k(key, opts) :: list(atom)
+  @spec sigil_k(key, opts) :: [atom]
   def sigil_k(key, _opts) when is_binary(key) do
     key |> String.split(".") |> Enum.map(&String.to_atom(&1))
   end

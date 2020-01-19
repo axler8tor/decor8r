b9a8ba2593eecf252dd44220b47bcfba774eadad
axler8tor <axl.mattheus@gmail.com>
2020-01-20 10:38:15 +1300

    Updated semver release number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Decorator.MixProject do
   def project do
     [
       app: :decorator,
-      version: "0.1.0",
+      version: "0.2.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

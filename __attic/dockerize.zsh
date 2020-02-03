660d2855709aa97b6308757b1c78d06edb44a8d1
axler8tor <axl.mattheus@gmail.com>
2020-02-04 09:07:33 +1300

    Remove attic

--- __attic/dockerize.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-#set -eu
-set now=$(date +%Y%m%d)
-set rnd=$(openssl rand -hex 3)
-version="0.1.${now}${rnd}"
-docker build -t decor8r:${version} .
-#docker run --rm --interactive --tty --volume $PWD:/test decor8r:${version} "$@"

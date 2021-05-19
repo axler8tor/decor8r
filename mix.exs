da4a926502f8c11dede73706aca29cd0e81fcd17
AxlER8R <axl.mattheus@gmail.com>
2021-05-19 23:21:32 +1200

    Update mix

--- mix.exs
+++ mix.exs
@@ -3,15 +3,25 @@ defmodule Decorator.MixProject do
 
   def project do
     [
-      app: :decorator,
-      version: "0.1.0",
+      app: :decor8r,
+      version: "0.2.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+
+      # QC
       aliases: aliases(),
       dialyzer: [
         plt_file: {:no_warn, "_dialyzer/plts/dialyzer.plt"},
         ignore_warnings: ".dialyzer_ignore.exs"
+      ],
+
+      # Documentation
+      source_url: "https://github.com/axler8tor/decor8r",
+      docs: [
+        extras: [
+          "README.md"
+        ]
       ]
     ]
   end
@@ -25,14 +35,16 @@ defmodule Decorator.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+      {:credo, "~> 1.2.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:toml, "~> 0.6.1"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
   defp aliases() do
     [
-      qc: ["format", "credo --strict", "dialyzer"]
+      qc: ["format", "dialyzer", "credo --strict"]
     ]
   end
 end

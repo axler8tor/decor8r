5e1449ccd56106cb5c81b135ef6adb24f1e2d8cb
AxlER8R <axl.mattheus@gmail.com>
2021-05-24 22:54:21 +1200

    Implement path decorator

--- backend/lib/decorator/terminal/path_decorator.ex
+++ backend/lib/decorator/terminal/path_decorator.ex
@@ -1,13 +1,21 @@
-defmodule Decorator.Terminal.Directory do
+defmodule Decorator.Terminal.PathDecorator do
+  @moduledoc false
+
+  defstruct background: IO.ANSI.normal(),
+            foreground: IO.ANSI.normal(),
+            compact: :true,
+            boldlast: :true,
+            softstop: ""
+
   @spec decorate(binary) :: binary
-  def decorate(directory) do
-    directory
+  def decorate(path) do
+    path
     |> String.split(~S[/], trim: true)
     |> Enum.join("  ")
     |> do_decorate()
   end
 
-  defp do_decorate(directory) do
-    IO.ANSI.blue_background() <> IO.ANSI.white() <> directory <> IO.ANSI.reset()
+  defp do_decorate(path) do
+    IO.ANSI.blue_background() <> IO.ANSI.white() <> path <> IO.ANSI.reset()
   end
 end

e01b0d5babdb16386d2855af4c5b5db4613016dd
AxlER8R <axl@axler8r.io>
2021-06-02 10:31:05 +1200

    Expand configuration helper utilities

--- backend/lib/decorator/configuration.ex
+++ backend/lib/decorator/configuration.ex
@@ -1,15 +1,30 @@
 defmodule Decorator.Configuration do
+  @moduledoc """
+  Load, present and reload configuration information.
+  """
   use GenServer
 
   # API
-  @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
-  def start_link(_) do
+  @spec start_link() :: :ignore | {:error, any()} | {:ok, pid()}
+  @spec start_link(any()) :: :ignore | {:error, any()} | {:ok, pid()}
+  def start_link(_state\\nil) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  @spec get() :: any()
+  @doc """
+  Reteive configuration map.
+  """
+  @spec get() :: map()
   def get() do
     GenServer.call(__MODULE__, :get)
+  end
+
+  @doc """
+  Reteive specific configuration as specified by key path.
+  """
+  @spec get_at(list(atom)) :: any()
+  def get_at(key) do
+    Kernel.get_in(get(), key)
   end
 
   # Callbacks
@@ -22,5 +37,29 @@ defmodule Decorator.Configuration do
   @impl true
   def handle_call(:get, _from, config) do
     {:reply, config, config}
+  end
+end
+
+defmodule Decorator.Configuration.Key do
+  @moduledoc """
+  Helpers to simplify access to configuration data
+  """
+
+  @doc """
+  TODO
+  """
+  @spec sigil_k(binary()) :: list(atom)
+  @spec sigil_k(binary(), list()) :: list(atom)
+  def sigil_k(key, _options\\[]), do: key |> to_atoms()
+
+  @doc """
+  TODO
+  """
+  @spec to_atoms(binary()) :: list(atom)
+  @spec to_atoms(binary(), list()) :: list(atom)
+  def to_atoms(key, _options\\[]) do
+    key
+    |> String.split(".")
+    |> Enum.map(&String.to_atom/1)
   end
 end

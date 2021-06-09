b7f3bee3b438e15fb7fa4e1692f9cc68d1c40872
AxlER8R <axl@axler8r.io>
2021-06-09 14:31:21 +1200

    Add source code

---
+++ Source/DecoR8R.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace decor8r
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
